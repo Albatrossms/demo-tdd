17d5f10e4492356afe4b60d0840ee06012fe7d7a
Saiju Stephen <saijumathew@gmail.com>
2021-01-14 13:56:57 -0500

    test

---
+++ src/Components/TodoList.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const TodoList = ({todos}) => <div></div>;
+export default TodoList;
---
+++ src/mockData.js
@@ -0,0 +1,8 @@
+const mockData = [{
+    userld: 1, id: 1, title: 'Eat breakfast', completed: false,
+    userld: 1, id: 2, title: 'Do laundry', completed: false,
+    userld: 1, id: 3, title: 'Take out the trash', completed: false,
+    userld: 1, id: 4, title: 'Write a blog post', completed: true,
+}, { userld: 1, id: 5, title: 'Go out for a walk', completed: false, },
+];
+export default mockData; 
