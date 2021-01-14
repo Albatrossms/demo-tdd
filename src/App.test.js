b8f81105096b50c00f1ed9830980b70567422f06
Saiju <saijumathew@gmail.com>
2021-01-14 18:38:59 -0500

    batch 2
    batch 2

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { render } from '@testing-library/react';
 import App from './App';
+import mockData from './mockData'
+
+beforeEach(()=>{
+    fetchMock.once(JSON.stringify(mockData));
+});
 
 describe(
     '<app/> test', () => {
